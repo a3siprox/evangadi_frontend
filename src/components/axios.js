55a75a7bb196877adaaf93010d56839893ba81f1
a3siprox <abdulmejidsetegn4@gmail.com>
2024-08-21 21:17:22 +0300

    deploy frontend

--- src/components/axios.js
+++ src/components/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5500/api/",
-  baseURL: "https://evangadi-backend-qh9f.onrender.com/",
+  baseURL: "https://evangadi-backend-qh9f.onrender.com/api/",
 });
 export default axiosBase;
